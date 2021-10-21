1bb4d7d4252d2be6862730c421b2282a7e52eb2f
Melody Michard <melodym.michard@gmail.com>
2021-10-21 14:55:03 -0600

    init commit

---
+++ Loop Challenges.js
@@ -0,0 +1,30 @@
+
+for (let i=1; i < 20; i+=2) {
+        console.log(i);
+}
+
+
+for (let i=100; i > -1; i--) {
+    if (i % 3 == 0);
+        console.log(i);
+}
+    
+
+for (let i=4; i>-4; i-=1.5) {
+    console.log(i);
+}
+
+
+let sum = 0;
+for (let i=1; i<101; i++) {
+    sum += i;
+    console.log(sum);
+}
+
+
+let total = 1;
+for (let i=1; i<13; i++) {
+    total *= i;
+    console.log(total);
+}
+
